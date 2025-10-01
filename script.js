20dd1e31cef454a60056083c7ee837ce1878b017
EffieNeu <effieneuman@gmail.com>
2025-10-01 13:41:48 -0400

    Clasherdle V1
    Added Clasherdle will deffinetly make improvments soon just a mock up. got rid of a bad readme and added header to home page

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 const quotes = [
       "The best revenge is massive success.",
-      "Creativity is intelligence having fun. – Einstein (probably)",
+      "Creativity is intelligence having fun. ï¿½ Einstein (probably)",
       "404: Quote not found. Try again tomorrow.",
       "Those who think they can and those who think they can't are both usually right",
       "You do not fall to the value of your goals, you fall to the value of your system.",
       "Obsessed is just a word the lazy use to describe dedication",
       "Common sense is like deodrant, the people who need it most never use it",
-      "Don’t watch the clock; do what it does. Keep going.",
-      "Normality is a paved road: It’s comfortable to walk, but no flowers grow there.",
+      "Donï¿½t watch the clock; do what it does. Keep going.",
+      "Normality is a paved road: Itï¿½s comfortable to walk, but no flowers grow there.",
       "Some people want it to happen, some wish it would happen, others make it happen.",
       "If you want to fly, give up everything that weighs you down.",
       "Winners are not people who never fail, but people who never quit.",
-      "It always seems impossible until it’s done.",
+      "It always seems impossible until itï¿½s done.",
       "If you judge a fish by its ability to climb a tree it will live its whole life beliving hes an idiot"
     ];
 
